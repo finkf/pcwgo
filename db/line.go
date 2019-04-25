587c84aa9b4113f7e29df43fffeaaa7c38be6951
flo <flo@cis.lmu.de>
2019-04-25 10:55:09 +0200

    Handle error from query

--- db/line.go
+++ db/line.go
@@ -179,6 +179,9 @@ func FindLineByID(db DB, bookID, pageID, lineID int) (*Line, bool, error) {
 
 	// query for contents
 	rows, err = Query(db, stmt2, bookID, pageID, lineID)
+	if err != nil {
+		return nil, false, err
+	}
 	for rows.Next() {
 		line.Chars = append(line.Chars, Char{})
 		if err := scanChar(rows, &line.Chars[len(line.Chars)-1]); err != nil {
